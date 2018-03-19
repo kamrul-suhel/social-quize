cee5b08761a6dd123ecd64c0f6e2e82f2a9ecd73
Ian Lainchbury <>
2018-03-19 12:07:43 +0000

    Fixing and updating errors

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -9,38 +9,40 @@
    }
 </script>
 <style scoped>
+  .question{
+      width: 90%;
+      margin:0 auto;
+      padding-top:130px;
+      min-height:430px;
+  }
+  .animation-heading {
+      -webkit-animation-fill-mode: forwards;
+      -moz-animation-fill-mode: forwards;
+      -o-animation-fill-mode: forwards;
+      -ms-animation-fill-mode: forwards;
+      animation-fill-mode: forwards;
+      -webkit-animation-delay: .5s;
+      -moz-animation-delay: .5s;
+      -o-animation-delay: .5s;
+      animation-delay: .5s;
+      animation-duration: 1.5s;
+      -moz-animation-duration: 1.5s;
+      -webkit-animation-duration: 1.5s;
+      -o-animation-duration: 1.5s;
+      animation-duration: 1.5s;
+      -webkit-animation-name: animation-heading;
+      -moz-animation-name: animation-heading;
+      -o-animation-name: animation-heading;
+      animation-name: animation-heading;
 
-.question{
-    margin-top:150px;
-}
-    .animation-heading {
-        -webkit-animation-fill-mode: forwards;
-        -moz-animation-fill-mode: forwards;
-        -o-animation-fill-mode: forwards;
-        -ms-animation-fill-mode: forwards;
-        animation-fill-mode: forwards;
-        -webkit-animation-delay: .5s;
-        -moz-animation-delay: .5s;
-        -o-animation-delay: .5s;
-        animation-delay: .5s;
-        animation-duration: 1.5s;
-        -moz-animation-duration: 1.5s;
-        -webkit-animation-duration: 1.5s;
-        -o-animation-duration: 1.5s;
-        animation-duration: 1.5s;
-        -webkit-animation-name: animation-heading;
-        -moz-animation-name: animation-heading;
-        -o-animation-name: animation-heading;
-        animation-name: animation-heading;
+  }
 
-    }
-
-    @keyframes animation-heading {
-        from{
-            opacity:0;
-        },
-        to{
-            opacity:1;
-        }
-    }
+  @keyframes animation-heading {
+      from{
+          opacity:0;
+      },
+      to{
+          opacity:1;
+      }
+  }
 </style>
